afe332f50a14d81e2529c2d4ee80a3542724a732
moloud <moloud.ayat@gmail.com>
2024-09-03 20:54:55 +0330

    feat: basic config

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,18 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function Layout() {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+const Onboarding = () => {
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between">
+      <TouchableOpacity
+        onPress={() => router.replace("/(auth)/sign-up")}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+export default Onboarding;
