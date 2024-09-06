572aa6140dd83b9c1673fc6c373bc5e2a84328ab
moloud <moloud.ayat@gmail.com>
2024-09-06 21:19:41 +0330

    feat(login): add session

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { View, Text, ScrollView, Image } from "react-native";
 import { images, icons } from "@/constants";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import { useSignIn } from "@clerk/clerk-expo";
 import InputField from "@/components/InputField";
 import CustomButton from "@/components/CustomButton";
 import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const router = useRouter();
+  const { signIn, setActive, isLoaded } = useSignIn();
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  const onSignInPress = async () => {};
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        // See https://clerk.com/docs/custom-flows/error-handling
+        // for more info on error handling
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form]);
+
   return (
     <ScrollView className="flex-1 bg-white">
       <View className="flex-1 bg-white">
@@ -44,13 +70,13 @@ const SignIn = () => {
             className="mt-6"
           />
           <OAuth />
-          {/* <Link
+          <Link
             href="/sign-up"
             className="text-lg text-center text-general-200 mt-20"
           >
             <Text>Don't have an account? </Text>
             <Text className="text-primary-500">Sign Up</Text>
-          </Link> */}
+          </Link>
         </View>
         {/** Verification Modal */}
       </View>
