6e116156c22f09ee93845d6bb521994acba79612
moloud <moloud.ayat@gmail.com>
2024-09-06 18:23:19 +0330

    feat(SignUp): add clerk and sign up senario

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,60 @@
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import { images, icons } from "@/constants";
+import { Link } from "expo-router";
+import InputField from "@/components/InputField";
+import CustomButton from "@/components/CustomButton";
+import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = async () => {};
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View>
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+          <OAuth />
+          {/* <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-20"
+          >
+            <Text>Don't have an account? </Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link> */}
+        </View>
+        {/** Verification Modal */}
+      </View>
+    </ScrollView>
   );
 };
 export default SignIn;
